7328a1aff414a7e4ccf3f45be282516804f61ecc
BrantTanner <brant.tanner123@gmail.com>
2026-03-10 09:45:31 -0600

    updated js

--- teamFinal/characterGuesser.js
+++ teamFinal/characterGuesser.js
@@ -17,24 +17,39 @@ function startGame(){
     const randCharacterIndex = Math.floor(Math.random() * characters.length + 1);
     const characterImage = characters[randCharacterIndex].characterImage;
     const characterHint = characters[randCharacterIndex].characterHint;
+    const characterName = characters[randCharacterIndex].name;
 
     // Grab character image and hint from html
-    const img = document.querySelector(".characterAndHint  img");
-    const hint = document.querySelector(".characterAndHint p")
+    const img = document.querySelector(".characterAndHint img");
+    const hint = document.querySelector(".characterAndHint p");
 
     img.src = characterImage;
-    hint.textContent = characterHint
+    hint.textContent = "Hint: " + characterHint
 
     
-    // handle form input and guess
+    // Handle form input and guess
     const form = document.querySelector('form');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
         const input = document.getElementById('playerGuess');
         const userGuess = input.value;
+        
+        console.log(characterName)
         console.log(userGuess);
+
+        if(userGuess.toLowerCase() == characterName.toLowerCase()){
+            alert("You guessed right!")
+        }
+        else{
+            alert("Wrong! Try again, The answer was " + characterName)
+        }
+
+        location.reload();
+        
     })
+
+
     
 }
 
