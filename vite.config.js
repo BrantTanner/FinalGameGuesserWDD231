9c882f96f8608609ffe8aa19c327fae82f69672b
BrantTanner <brant.tanner123@gmail.com>
2026-03-31 15:43:54 -0600

    Vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite'
+import { dirname, resolve } from "path";
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: './teamFinal',
@@ -9,5 +13,19 @@ export default defineConfig({
   preview: {
     port: 4173,
     open: true
+  },
+
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "teamFinal/homepage/index.html"),
+        characterGuesser: resolve(__dirname, "teamFinal/characterGuesser/characterGuesser.html"),
+        contactUs: resolve(__dirname, "teamFinal/contactUs/contactUs.html"),
+        credits: resolve(__dirname, "teamFinal/creditsPage/credits.html"),
+        rules: resolve(__dirname, "teamFinal/rules.html")
+      }
+    }
   }
 })
